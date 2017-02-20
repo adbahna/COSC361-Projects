2a748543ed77eff40c3c2a3505328dae3863118e
jillbore <mike.goin12@gmail.com>
2017-02-19 20:10:32 -0500

    Added mutex and semaphores

--- Project2/sched.cpp
+++ Project2/sched.cpp
@@ -1,5 +1,9 @@
 #include "sched.h"
 #include "string.h"
+#include "limits.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 //Simulate a timer interrupt from hardware. This should initiate
 //the context switch procedure
@@ -12,35 +16,59 @@ void timer_interrupt(SCHEDULER *s) {
         //Interactive scheduling
         case SA_ROUND_ROBIN:
             i = s->current + 1;
-            while(1)
+            while(i != s->current)
             {
                 if (i == MAX_PROCESSES - 1)
-                    i = 1;
-                else if (s->process_list[i].state == PS_NONE)
+                    i = 0;
+                else if (s->process_list[i].state != PS_RUNNING)
                     i++;
-                else {
+                else
+                    break;
+            }
+            newProcess = i;
+            break;
+        case SA_FAIR:
+            // pick the lowest of each process'  total_cpu_time / switched_cpu_time
+            minTime = INT_MAX;
+            for (i = 0; i < MAX_PROCESSES; i++) {
+                if (s->process_list[i].state == PS_RUNNING) {
+                    if (s->process_list[i].total_cpu_time == 0) {
+                        minTime = 0;
+                        newProcess = i;
+                    }
+                    else if (s->process_list[i].switched_cpu_time == 0) {
+                        if (minTime == INT_MAX) {
+                            newProcess = i;
+                        }
+                    }
+                    else if ((s->process_list[i].total_cpu_time / s->process_list[i].switched_cpu_time) < minTime) {
+                        minTime = (s->process_list[i].total_cpu_time / s->process_list[i].switched_cpu_time);
+                        newProcess = i;
+                    }
+                }
+            }
+            break;
+        //Batch scheduling
+        case SA_FCFS:
+            for (i = 0; i < MAX_PROCESSES; i++) {
+                if (s->process_list[i].state == PS_RUNNING) {
                     newProcess = i;
                     break;
                 }
             }
             break;
-        case SA_FAIR:
-            // pick the lowest of each process'  total_cpu_time / switched_cpu_time
-			minTime = 10000000;
-
-			for (i = 1; i < MAX_PROCESSES; i++)
-			{
-				if ((s->process_list[i].total_cpu_time / s->process_list[i].switched_cpu_time) < minTime)
-				{
-					 minTime = (s->process_list[i].total_cpu_time / s->process_list[i].switched_cpu_time);	
-					 newProcess = i;
-				}
-			}
-
-            break;
-        case SA_FCFS:
-            break;
         case SA_SJF:
+            minTime = INT_MAX;
+            for (i = 0; i < MAX_PROCESSES; i++)
+            {
+                if (s->process_list[i].state == PS_RUNNING && s->process_list[i].job_time != -1) {
+                    if (s->process_list[i].job_time < minTime)
+                    {
+                        minTime = s->process_list[i].job_time;
+                        newProcess = i;
+                    }
+                }
+            }
             break;
     }
 
@@ -48,23 +76,54 @@ void timer_interrupt(SCHEDULER *s) {
     s->process_list[s->current].saved_registers = s->active_registers;
     s->active_registers = s->process_list[newProcess].saved_registers;
 
-    s->process_list[i].switched++;
+    s->current = newProcess;
+    s->process_list[newProcess].switched++;
 
-    // Call either init or step, based on the context switch numbers
-    if (s->process_list[i].switched == 1) {
-
-    }
-
+    printf("Pid %d: switched %d\n",newProcess,s->process_list[newProcess].switched);
+    RETURN rv;
     // Call either init (context switch count == 1), or step (context switch count > 1)
-    // Read RETURN structure
-    // Update process’ state based on the RETURN structure
+    if (s->process_list[newProcess].switched == 1)
+        s->process_list[newProcess].init(&s->active_registers, &rv);
+    else
+        s->process_list[newProcess].step(&s->active_registers, &rv);
 
+    // Update process’ state based on the RETURN structure
+    s->process_list[newProcess].state = rv.state;
+    s->process_list[newProcess].total_cpu_time += rv.cpu_time_taken;
+    s->process_list[newProcess].sleep_time_remaining = rv.sleep_time;
+
+    for (i = 0; i < MAX_PROCESSES; i++) {
+        // Add to the switched_cpu_time of all the other processes
+        if (s->process_list[i].state == PS_RUNNING)
+            s->process_list[i].switched_cpu_time += rv.cpu_time_taken;
+
+        // Decrement the sleep time for all the processes who are sleeping
+        if (s->process_list[i].state == PS_SLEEPING) {
+            // If a process is done sleeping, make it run
+            if (s->process_list[i].sleep_time_remaining <= rv.cpu_time_taken) {
+                s->process_list[i].sleep_time_remaining = 0;
+                s->process_list[i].state = PS_RUNNING;
+            }
+            // Otherwise substract from sleep time
+            else
+                s->process_list[i].sleep_time_remaining -= rv.cpu_time_taken;
+        }
+    }
 }
 
 // Create a new scheduler
 SCHEDULER *new_scheduler(PROCESS_CODE_PTR(code)) {
     // Create a new scheduler (on heap)
     SCHEDULER *s = new SCHEDULER;
+
+    for (int i = 0; i < MAX_MUTEX; i++)
+        s->mutex_list[i] = -1;
+    for (int i = 0; i < MAX_SEM; i++)
+        s->sem_list[i] = -1;
+
+    for (int i = 0; i < MAX_PROCESSES; i++)
+        s->process_list[i].state = PS_NONE;
+
     // Schedule process 1 (init)
     s->process_list[1].name = (char*)"init";
     s->process_list[1].pid = 1;
@@ -119,4 +178,62 @@ int exec(SCHEDULER *s, PID pid, const char *new_name, PROCESS_CODE_PTR(init), PR
     s->process_list[pid].step = step;
 
     return 0;
+}
+
+
+//Create or destroy a mutex in the scheduler
+//mutex_create returns the ID of the new mutex, or -1 if
+//there are no more allocations
+MUTEX mutex_create(SCHEDULER *s) {
+    int mutexID = -1;
+    for (int i = 0; i < MAX_MUTEX; i++) {
+        if (s->mutex_list[i] == -1) {
+            mutexID = i;
+            s->mutex_list[i] = 0;
+            break;
+        }
+    }
+    return mutexID;
+}
+void mutex_destroy(SCHEDULER *s, MUTEX m) {
+    s->mutex_list[m] = -1;
+}
+//Lock or unlock a mutex given by the mutex index m
+//If a lock cannot be granted, return 0, otherwise return 1
+int mutex_lock(SCHEDULER *s, MUTEX m) {
+    if (s->mutex_list[m] != 0)
+        return 0;
+
+    s->mutex_list[m] = 1;
+    return 1;
+}
+void mutex_unlock(SCHEDULER *s, MUTEX m) {
+    s->mutex_list[m] = 0;
+}
+
+//Create or destroy a semaphore in the scheduler
+//sem_create returns the ID of the new semaphore, or -1 if
+//there are no more allocations.
+SEMAPHORE sem_create(SCHEDULER *s) {
+    int semID = -1;
+    for (int i = 0; i < MAX_SEM; i++) {
+        if (s->sem_list[i] == -1) {
+            semID = i;
+            s->sem_list[i] = 0;
+            break;
+        }
+    }
+    return semID;
+}
+void sem_destroy(SCHEDULER *s, SEMAPHORE m) {
+    s->sem_list[m] = -1;
+}
+//Raises or lowers the semaphore number of semaphore m
+void sem_up(SCHEDULER *s, SEMAPHORE m) {
+    s->sem_list[m]++;
+}
+int sem_down(SCHEDULER *s, SEMAPHORE m) {
+    if (s->sem_list[m] > 0)
+        s->sem_list[m]--;
+    return s->sem_list[m];
 }
